443c5e4fbc93c9140c4d330fd8d8fe2679648003
diegot-code <torres.db04@gmail.com>
2025-01-17 16:06:52 -0500

    Added header template

--- python/module_1.php
+++ python/module_1.php
@@ -26,32 +26,11 @@ $lessons = [
   "lesson7" => "Details",
   "lesson8" => "Details"
 ];
+$pageName = "Python Module 1";
 
+include_once "../temp_header.php";
 ?>
 
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <link rel="stylesheet" href="../styles.css" />
-
-    <title>Quick Start</title>
-  </head>
-  <body>
-    <header>
-      <div class="container flex space-between">
-        <div>
-          JAX<span>CODE</span> DOCS <br />
-          Home / Syllabus MyDocs <br />
-          <a href="../">Back</a>
-        </div>
-        <h1 class="">Python</h1>
-
-      </div>
-    </header>
     <main id="lessons">
     <?php foreach ($lessons as $lessonTitle => $details) {
         ?>
